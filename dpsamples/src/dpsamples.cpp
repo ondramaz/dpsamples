ba25fe3753237175b1be859576bbca234133bf74
Ondrej Mazanec <ondrej.mazanec@gmail.com>
2018-01-06 21:26:15 +0100

    init

---
+++ dpsamples/src/dpsamples.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : dpsamples.cpp
+// Author      : Ondrej Mazanec
+// Version     :
+// Copyright   : 
+// Description : Design pattern samples in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
